58cc9330aa78023e2f580e549c87296380bc2d40
NRN\lizhou <lingjun.zhou@nrcan-rncan.gc.ca>
2022-06-24 13:25:33 -0500

    updated procfile

--- app/deploy/deploy.sh
+++
@@ -1,9 +0,0 @@
-full_path=$(realpath $0)
-dir_path=$(dirname $full_path)
-root_path=$(dirname $dir_path)
-# data_path=$(dirname $root_path)/data/df2_processed.csv
-# # echo $data_path
-
-# python deploy/deploy.py
-cd $root_path/deploy
-uvicorn deploy:app --reload --port 8001
