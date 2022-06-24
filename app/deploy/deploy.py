8bae60bde2fcc7e91ac96ea3a06a8211ab654311
NRN\lizhou <lingjun.zhou@nrcan-rncan.gc.ca>
2022-06-24 15:15:11 -0500

    ..

--- app/deploy/deploy.py
+++ app/deploy/deploy.py
@@ -3,6 +3,7 @@ import uvicorn
 from joblib import load
 from fastapi import FastAPI
 import os
+import neptune.new as neptune
 
 app = FastAPI(title="Remote Community Power Usage")
 
@@ -12,17 +13,25 @@ model_unvailable_msg="Model NOT Available"
 async def root():
     return {"message": "Remote Community Power Usage"}
 
+file_path = os.path.realpath(__file__)
+dir_path = os.path.dirname(file_path)
+root_path = os.path.dirname(dir_path)
+
+def fetch_model(path):
+    run = neptune.init(project="lingjun/Remote-Community-Power-Usage", 
+    api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI2ZDQ3M2M3Yy0xYjNkLTRmMDYtOTYyNi00ODE4ZjgzZjA2MGMifQ==",
+    run="REM-19")
+    run["model/pickled_model"].download(root_path+path)
+    print("MODEL FETCHED!")
+
+fetch_model(path="/artifacts/models")
+
 @app.post("/predict", status_code=200)
 async def predict(fh: int):
-    """
-    Make predictions with the model
-    """
     try:
-        file_path = os.path.realpath(__file__)
-        dir_path = os.path.dirname(file_path)
-        root_path = os.path.dirname(dir_path)
         # we can also download the model from a remote location and load it
-        model_path = root_path+"/artifacts/models/lgbm_forecaster.pickle"
+        model_path = root_path+"/artifacts/models/pickled_model.pkl"
+        # joblib can load .pkl model
         model = load(model_path)
     except:
         print("Model not available")
