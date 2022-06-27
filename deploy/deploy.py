a28bf10899e4f53edaaeca452b26d67ced96f7d7
NRN\lizhou <lingjun.zhou@nrcan-rncan.gc.ca>
2022-06-27 16:06:50 -0500

    no message

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -4,46 +4,49 @@ from joblib import load
 from fastapi import FastAPI
 import os
 
-file_path = os.path.realpath(__file__)
-dir_path = os.path.dirname(file_path)
-root_path = os.path.dirname(dir_path)
+
 
 app = FastAPI(title="Remote Community Power Usage")
 
 model_unvailable_msg="Model NOT Available"
 
 
-model_path = root_path+"/artifacts/models/pickled_model.pkl"
-model = load(model_path)
-results = model.predict(fh=2)
-print(results)
+# ================================ Test Code =============================================================
+# file_path = os.path.realpath(__file__)
+# dir_path = os.path.dirname(file_path)
+# root_path = os.path.dirname(dir_path)
+# model_path = root_path+"/artifacts/models/pickled_model.pkl"
+# model = load(model_path)
+# results = model.predict(fh=2)
+# print(results)
+# ================================ Test Code =============================================================
 
-# @app.get("/")
-# async def root():
-#     return {"message": "Remote Community Power Usage"}
+@app.get("/")
+async def root():
+    return {"message": "Remote Community Power Usage"}
 
-# @app.post("/predict", status_code=200)
-# async def predict(fh: int):
-#     """
-#     Make predictions with the model
-#     """
-#     try:
-#         file_path = os.path.realpath(__file__)
-#         dir_path = os.path.dirname(file_path)
-#         root_path = os.path.dirname(dir_path)
-#         # we can also download the model from a remote location and load it
-#         model_path = root_path+"/artifacts/models/lgbm_forecaster.pickle"
-#         model = load(model_path)
-#     except:
-#         print("Model not available")
-#         return model_unvailable_msg
-#     results = model.predict(fh=fh)
+@app.post("/predict", status_code=200)
+async def predict(fh: int):
+    """
+    Make predictions with the model
+    """
+    try:
+        file_path = os.path.realpath(__file__)
+        dir_path = os.path.dirname(file_path)
+        root_path = os.path.dirname(dir_path)
+        # we can also download the model from a remote location and load it
+        model_path = root_path+"/artifacts/models/lgbm_forecaster.pickle"
+        model = load(model_path)
+    except:
+        print("Model not available")
+        return model_unvailable_msg
+    results = model.predict(fh=fh)
 
-#     return results
+    return results
 
 
 
-# if __name__ == "__main__":
-#     # Use this for debugging purposes only
-#     # reload=True appears to be broken in uvicorn.run(), but not in CLI
-#     uvicorn.run(app, host="localhost", port=8001, log_level="debug", reload=False)
+if __name__ == "__main__":
+    # Use this for debugging purposes only
+    # reload=True appears to be broken in uvicorn.run(), but not in CLI
+    uvicorn.run(app, host="localhost", port=8001, log_level="debug", reload=False)
